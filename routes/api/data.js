4a19ecf381bb1a798d8023cb87bff778c9c73de5
Jemmy <jemtatt@outlook.com>
2019-03-17 12:31:02 -0600

    fixed the year from date.

--- routes/api/data.js
+++ routes/api/data.js
@@ -1,31 +1,14 @@
 const router = require('express').Router();
-const donorsController = require('../../controllers/donorsController');
+// const donorsController = require('../../controllers/donorsController');
 const db = require("../../models");
 
-// router
-// .route('/')
-//console.log('this route');
-// .get(donorsController.campaignSearch);
-
-// router.route('/donors').get(donorsController.campaignSearchResults);
-
-// router.get('/donors', function(req, res){
-//         // console.log('this route');
-//         // db.Data.find({}).then(response => {
-//         //         const truncatedResponse = [...new Set(response.map(campaignDonor=>campaignDonor))].filter(item=>item !== 'NULL').sort((a,b)=> a < b ? -1 : 0);
-//         //         res.json({response: truncatedResponse});
-//         // }).catch(err=> {
-//         //         console.log(err);
-//         // })
-// });
-//         // .post(donorsController.create);
 
 router.get('/', function (req, res) {
 	console.log('this route');
 	let result = {}
 	db.Data.find({}).then(response => {
 		result[0] = [...new Set(response.map(campaignDonor => campaignDonor.campaignName))].filter(item => item !== 'NULL').sort((a, b) => a < b ? -1 : 1);
-		result[1] = [...new Set(response.map(campaignDonor => campaignDonor.date))].filter(item => item !== 'NULL').sort((a, b) => a < b ? -1 : 1);
+		result[1] = [...new Set(response.map(campaignDonor => campaignDonor.year))].filter(item => item !== 'NULL').sort((a, b) => a < b ? -1 : 1);
 		result[2] = [...new Set(response.map(campaignDonor => campaignDonor.race))].filter(item => item !== 'NULL' && item !== 'null').sort((a, b) => a < b ? -1 : 1);
 		result[3] = [...new Set(response.map(campaignDonor => campaignDonor.amount))].filter(item => item !== 'NULL').sort((a, b) => a < b ? -1 : 1);
 		res.json({ response: result });
@@ -57,7 +40,7 @@ router.post('/contributedinfo', function (req, res) {
 
 	db.Data.aggregate([
 		{ $match: { race: req.body.params.race } },
-		{ $group: { _id: "$date", total: { $sum: "$amount" } } }
+		{ $group: { _id: "$year", total: { $sum: "$amount" } } }
 	]).then(response => {
 		res.json({ response });
 		console.log(response);
@@ -79,13 +62,13 @@ router.post('/contributedinfo', function (req, res) {
 router.get('/yearlycontributed', function (req, res) {
 	// let campaignName = req.query.campaignName;
 	let year = req.query.year;
-	let yearRegex = '^' + year;
+	// let yearRegex = '^' + year;
 	// let amount = req.query.amount;
 	// let minAmt = parseFloat(req.query.minAmt);
 	// let maxAmt = parseFloat(req.query.maxAmt);
 	console.log('this route: yearly Contributed' + " " + req.query.year);
 
-	db.Data.find({ date: { $regex: yearRegex } })
+	db.Data.find({ year: year })
 		.then(response => {
 			res.json({ response });
 			console.log(response);
@@ -95,10 +78,12 @@ router.get('/yearlycontributed', function (req, res) {
 });
 
 router.post('/yearlycontributedinfo', function (req, res) {
+	let year = req.body.params.year;
 
 	db.Data.find({
+
 		// campaignName: { $campaignName: req.body.campaignName },
-		date: { $regex: req.body.params.year },
+		year: year,
 		// amount: { $amount: req.body.amount }
 	})
 		.then(response => {
