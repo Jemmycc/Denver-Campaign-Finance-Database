5ad9d88f30f7508a766cd59aed058fe1ce22e420
Jemmy <jemtatt@outlook.com>
2019-05-07 21:52:30 -0600

    package json added engine info

---
+++ client/build/precache-manifest.32944ae0be0a1c7f9842a4ce3eec8bf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3ac95e07a02b33dcebf",
+    "url": "/static/js/main.d3ac95e0.chunk.js"
+  },
+  {
+    "revision": "9927285a01baf23822ed",
+    "url": "/static/js/1.9927285a.chunk.js"
+  },
+  {
+    "revision": "d3ac95e07a02b33dcebf",
+    "url": "/static/css/main.22968431.chunk.css"
+  },
+  {
+    "revision": "e7fc8741135e17d7ea3cc4a9df2ce034",
+    "url": "/index.html"
+  }
+];
